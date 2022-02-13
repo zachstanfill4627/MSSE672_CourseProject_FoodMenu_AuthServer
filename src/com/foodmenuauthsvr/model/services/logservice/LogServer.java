5a317dfbd3e95a079f8c33b8a5c3cfab2d3aae9b
zachstanfill4627 <57971003+zachstanfill4627@users.noreply.github.com>
2022-02-13 14:07:30 -0700

    Begin implementing use of applicationContext.xml

---
+++ src/com/foodmenuauthsvr/model/services/logservice/LogServer.java
@@ -0,0 +1,29 @@
+package com.foodmenuauthsvr.model.services.logservice;
+
+/**
+ * 
+ * @author zach
+ *
+ * Adapted from Prof. Ishmael, MSSE672, Regis University -- Example EPedigreeThreadedServers Application
+ */
+
+public class LogServer {
+
+	private String propFileLocation;
+	
+	
+	public void setPropFileLocation(String propFileLocation ){
+		 
+		 this.propFileLocation = propFileLocation;
+		 	 
+	 	} // end of propFileLocation
+	
+	public String getPropFile (String propFile){
+		
+		propFile = propFileLocation;
+		
+		return propFile;
+	}
+	
+	
+} // end of LogServer
